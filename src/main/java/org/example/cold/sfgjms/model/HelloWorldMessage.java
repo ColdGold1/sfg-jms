29e627cefb88e4dae354729d8ec97d148adba1f8
ColdGold1 <prrfaceface1@gmail.com>
2025-01-22 11:50:33 +0300

    added java object message class

---
+++ src/main/java/org/example/cold/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,23 @@
+package org.example.cold.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class HelloWorldMessage implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private UUID uuid;
+    private String message;
+}
